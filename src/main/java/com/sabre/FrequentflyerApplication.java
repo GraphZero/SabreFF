ba4436445ab05d69a38902c079927580ea1e374b
Andrzej <andrzejad@hotmail.com>
2017-10-26 19:19:24 +0200

    Initial Commit

---
+++ src/main/java/com/sabre/FrequentflyerApplication.java
@@ -0,0 +1,12 @@
+package com.sabre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrequentflyerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrequentflyerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sabre/FrequentflyerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sabre;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FrequentflyerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
